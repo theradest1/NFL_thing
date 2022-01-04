953e02b62d847655798c194ce856e4031df05dcc
TheRad1 <59918992+theradest1@users.noreply.github.com>
2022-01-04 02:26:54 +0000

    A TON of stuffs. Almost done with the entire second try and it is going wayyyyyy better then first try mostly from organization and more planning and me knowing what I need to do.

--- main.py
+++ main.py
@@ -3,73 +3,46 @@ import itertools
 from operator import itemgetter
 import math
 
-#hey (;
-
-def setPoints():
-  for teamStats in allTeamStats:
-    teamStats[1] = random.randrange(50) #input(teamStats[0]+ "'s points: ")
-
-def topCombinations():
-  combinationPoints = []
-  topPlayers = []
-  ii = 0
-  for combination in usedCombinations:
-    i = 0
-    for j in range(3):
-      i += allTeamStats[combination[j]][1]
-    combinationPoints.append(i + ii/10000)
-    ii += 1
-  combinationPoints.sort(reverse = True)
-  i = 0
-  j = 0
-  #print(combinationPoints)
-  while j <= 2:
-    topPlayers.append(combinationPoints[i])
-    if int(combinationPoints[i]) == int(combinationPoints[i + 1]):
-      print(int(combinationPoints[i]))
-      j -= 1
-    i += 1
-    j += 1
-  return topPlayers, combinationPoints
-
-def displayTop():
-    print("")
-    print("")
-    #print("player ID, Points, (team1: points, team2: points, team3: points)")
-    team1points = team2points = team3points = 5
-    team1 = team2 = team3 = "example team"
-    i = 0
-    print(usedCombinations)
-    for player in topPlayers:
-        ID = int(math.modf(player)[0] * 10000)
-        team1points = allTeamStats[usedCombinations[ID][0]][1]
-        team1 = allTeamStats[usedCombinations[ID][0]][0]
-        team2points = allTeamStats[usedCombinations[ID][1]][1]
-        team2 = allTeamStats[usedCombinations[ID][1]][0]
-        team3points = allTeamStats[usedCombinations[ID][2]][1]
-        team3 = allTeamStats[usedCombinations[ID][2]][0]
-        print("")
-        print("Player ID: " + str(ID) + ", Points: " + str(int(player)) + "   teams: " + team1 + ": " + str(team1points) + ", " + team2 + ": " + str(team2points) + ", " + team3 + ": " + str(team3points))
-        i += 1
-  
-
+#SETUP --------------------------------------------------------------
 #[[name, score], [name, score]]
-allTeamStats = [["Arizona Cardinals", 0],["Atlanta Falcons", 0],["Baltimore Ravens", 0],["Buffalo Bills", 0],["Carolina Panthers", 0],["Chicago Bears", 0],["Cincinnati Bengals", 0],["Cleveland Browns", 0],["Dallas Cowboys", 0],["Denver Broncos", 0],["Detroit Lions", 0],["Green Bay Packers", 0],["Houston Texans", 0],["Indianapolis Colts", 0],["Jacksonville Jaguars", 0],["Kansas City Chiefs", 0],["Las Vegas Raiders", 0],["Los Angeles Chargers", 0],["Los Angeles Rams", 0],["Miami Dolphins", 0],["Minnesota Vikings", 0],["New England Patriots", 0],["New Orleans Saints", 0],["New York Giants", 0],["New York Jets", 0],["Philadelphia Eagles", 0],["Pittsburgh Steelers", 0],["San Francisco 49ers", 0],["Seattle Seahawks", 0],["Tampa Bay Buccaneers", 0],["Tennessee Titans", 0],["Washington Football Team", 0]]
+allTeamStats = [["Arizona Cardinals", 0],["Atlanta Falcons", 0],["Baltimore Ravens", 0], ["Buffalo Bills", 0],["Carolina Panthers", 0],["Chicago Bears", 0]]#,["Cincinnati Bengals", 0],["Cleveland Browns", 0],["Dallas Cowboys", 0],["Denver Broncos", 0],["Detroit Lions", 0],["Green Bay Packers", 0],["Houston Texans", 0],["Indianapolis Colts", 0],["Jacksonville Jaguars", 0],["Kansas City Chiefs", 0],["Las Vegas Raiders", 0],["Los Angeles Chargers", 0],["Los Angeles Rams", 0],["Miami Dolphins", 0],["Minnesota Vikings", 0],["New England Patriots", 0],["New Orleans Saints", 0],["New York Giants", 0],["New York Jets", 0],["Philadelphia Eagles", 0],["Pittsburgh Steelers", 0],["San Francisco 49ers", 0],["Seattle Seahawks", 0],["Tampa Bay Buccaneers", 0],["Tennessee Titans", 0],["Washington Football Team", 0]]
 
-teamNames = []
-teams = []
-i = 0
-for teamStats in allTeamStats:
-  teamNames.append(teamStats[0])
-  teams.append(i)
-  i += 1
+players = 5 #the amount of players
+step = 0 #is re-declared lower, is just here to remind me that it is here
+weeks = 3 #the amount of weeks this goes on for (change to 18 when done with dev)
+playerScores = [] # [[234, 26, 234, 567], [235, 235, 235]...]
+for i in range(weeks):
+  playerScores.append([])
 
-combinations = list(itertools.combinations(teams, 3))
-usedCombinations = combinations
+#setting scores for all the teams (will need to be not random later)
+for teamInfo in allTeamStats:
+  teamInfo[1] = random.randrange(1, 100)
 
-setPoints()
-print(allTeamStats)
+#generate the list of all combinations
+combinations = list(itertools.combinations(range(len(allTeamStats)), 3))
 
-topPlayers, combinationPoints = topCombinations()
-print(topPlayers)
-displayTop()
+step = int(len(combinations)/weeks)#the amount of combinations skipped for every week (starts from the player's starting number) **this has to be less than combinations/weeks or the player could have multible of the same combinations (even though its basicly impossible)
+player_step = int((len(combinations) - weeks)/players)
+
+#debug
+print("Format: [Team_Name, Score] \n", allTeamStats)
+print("\n", len(combinations), "combinations")#"\n", combinations) -- display all combinations (a lot)
+print("\n# of players:", players)
+print("Per week step amount:", step)
+print("Per player step amount:", player_step, "\n")
+
+#MAIN --------------------------------------------------------------
+
+for player_ID in range(players):
+  print("\nPlayer_ID:", player_ID) #debug
+  for week_number in range(weeks): 
+    score = 0 #reset the score every time it loops over another player
+    combination_ID = player_step * player_ID + week_number * step #for every player adds a player_step, for every week it adds a step, making it so everyone has different combinations no matter what
+    if combination_ID >= len(combinations):
+      combination_ID -= len(combinations) #if the combination_ID will go over the length of the combination list, it loops back to the beginning
+    for team_ID in combinations[combination_ID]:
+      score += allTeamStats[team_ID][1] #combining all the scores of the teams in the combination
+    playerScores[week_number].append(score + player_ID / 10000) #add the score and the player_ID to a list so we can sort it later to find the winners
+    print("Comb_ID: " + str(combination_ID) + ",", combinations[combination_ID],"=", score) #debug
+
+print("\n", playerScores) #debug
