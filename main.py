191164c6d65a0d0aa34c72e85a8a8776d62a9c97
TheRad1 <59918992+theradest1@users.noreply.github.com>
2022-01-04 03:14:31 +0000

    Test push

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import itertools
 from operator import itemgetter
 import math
 
-#SETUP --------------------------------------------------------------
+#SETUP -------------------------------------------------------------
 #[[name, score], [name, score]]
 allTeamStats = [["Arizona Cardinals", 0],["Atlanta Falcons", 0],["Baltimore Ravens", 0], ["Buffalo Bills", 0],["Carolina Panthers", 0],["Chicago Bears", 0]]#,["Cincinnati Bengals", 0],["Cleveland Browns", 0],["Dallas Cowboys", 0],["Denver Broncos", 0],["Detroit Lions", 0],["Green Bay Packers", 0],["Houston Texans", 0],["Indianapolis Colts", 0],["Jacksonville Jaguars", 0],["Kansas City Chiefs", 0],["Las Vegas Raiders", 0],["Los Angeles Chargers", 0],["Los Angeles Rams", 0],["Miami Dolphins", 0],["Minnesota Vikings", 0],["New England Patriots", 0],["New Orleans Saints", 0],["New York Giants", 0],["New York Jets", 0],["Philadelphia Eagles", 0],["Pittsburgh Steelers", 0],["San Francisco 49ers", 0],["Seattle Seahawks", 0],["Tampa Bay Buccaneers", 0],["Tennessee Titans", 0],["Washington Football Team", 0]]
 players = 5 #the amount of players
