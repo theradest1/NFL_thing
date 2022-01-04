89e0209c6a375b9563b7f27bb141137b40ef7573
TheRad1 <59918992+theradest1@users.noreply.github.com>
2022-01-04 02:35:46 +0000

    Cleaned up so ugly main thing is hidden in a function

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ import math
 #SETUP --------------------------------------------------------------
 #[[name, score], [name, score]]
 allTeamStats = [["Arizona Cardinals", 0],["Atlanta Falcons", 0],["Baltimore Ravens", 0], ["Buffalo Bills", 0],["Carolina Panthers", 0],["Chicago Bears", 0]]#,["Cincinnati Bengals", 0],["Cleveland Browns", 0],["Dallas Cowboys", 0],["Denver Broncos", 0],["Detroit Lions", 0],["Green Bay Packers", 0],["Houston Texans", 0],["Indianapolis Colts", 0],["Jacksonville Jaguars", 0],["Kansas City Chiefs", 0],["Las Vegas Raiders", 0],["Los Angeles Chargers", 0],["Los Angeles Rams", 0],["Miami Dolphins", 0],["Minnesota Vikings", 0],["New England Patriots", 0],["New Orleans Saints", 0],["New York Giants", 0],["New York Jets", 0],["Philadelphia Eagles", 0],["Pittsburgh Steelers", 0],["San Francisco 49ers", 0],["Seattle Seahawks", 0],["Tampa Bay Buccaneers", 0],["Tennessee Titans", 0],["Washington Football Team", 0]]
-
 players = 5 #the amount of players
 step = 0 #is re-declared lower, is just here to remind me that it is here
 weeks = 3 #the amount of weeks this goes on for (change to 18 when done with dev)
 playerScores = [] # [[234, 26, 234, 567], [235, 235, 235]...]
 for i in range(weeks):
   playerScores.append([])
-
-#setting scores for all the teams (will need to be not random later)
+  #setting scores for all the teams (will need to be not random later)
 for teamInfo in allTeamStats:
   teamInfo[1] = random.randrange(1, 100)
 
@@ -33,16 +31,20 @@ print("Per player step amount:", player_step, "\n")
 
 #MAIN --------------------------------------------------------------
 
-for player_ID in range(players):
-  print("\nPlayer_ID:", player_ID) #debug
-  for week_number in range(weeks): 
-    score = 0 #reset the score every time it loops over another player
-    combination_ID = player_step * player_ID + week_number * step #for every player adds a player_step, for every week it adds a step, making it so everyone has different combinations no matter what
-    if combination_ID >= len(combinations):
-      combination_ID -= len(combinations) #if the combination_ID will go over the length of the combination list, it loops back to the beginning
-    for team_ID in combinations[combination_ID]:
-      score += allTeamStats[team_ID][1] #combining all the scores of the teams in the combination
-    playerScores[week_number].append(score + player_ID / 10000) #add the score and the player_ID to a list so we can sort it later to find the winners
-    print("Comb_ID: " + str(combination_ID) + ",", combinations[combination_ID],"=", score) #debug
+def main():
+  for player_ID in range(players):
+    print("\nPlayer_ID:", player_ID) #debug
+    for week_number in range(weeks): 
+      score = 0 #reset the score every time it loops over another player
+      combination_ID = player_step * player_ID + week_number * step #for every player adds a player_step, for every week it adds a step, making it so everyone has different combinations no matter what
+      if combination_ID >= len(combinations):
+        combination_ID -= len(combinations) #if the combination_ID will go over the length of the combination list, it loops back to the beginning
+      for team_ID in combinations[combination_ID]:
+        score += allTeamStats[team_ID][1] #combining all the scores of the teams in the combination
+      playerScores[week_number].append(score + player_ID / 10000) #add the score and the player_ID to a list so we can sort it later to find the winners
+      print("Comb_ID: " + str(combination_ID) + ",", combinations[combination_ID],"=", score) #debug
+  return(playerScores)
 
-print("\n", playerScores) #debug
+playerScores = main()
+print(playerScores)
+
