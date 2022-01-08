5a2e5c271e3951290e44f41a560f4be8f4daa9dd
TheRad1 <59918992+theradest1@users.noreply.github.com>
2022-01-08 04:33:29 +0000

    Just started fitting to real world use (and f printers)

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import math
 #[[name, score], [name, score]]
 allTeamStats = [["Arizona Cardinals", 0],["Atlanta Falcons", 0],["Baltimore Ravens", 0], ["Buffalo Bills", 0],["Carolina Panthers", 0],["Chicago Bears", 0],["Cincinnati Bengals", 0],["Cleveland Browns", 0],["Dallas Cowboys", 0],["Denver Broncos", 0],["Detroit Lions", 0],["Green Bay Packers", 0],["Houston Texans", 0],["Indianapolis Colts", 0],["Jacksonville Jaguars", 0],["Kansas City Chiefs", 0],["Las Vegas Raiders", 0],["Los Angeles Chargers", 0],["Los Angeles Rams", 0],["Miami Dolphins", 0],["Minnesota Vikings", 0],["New England Patriots", 0],["New Orleans Saints", 0],["New York Giants", 0],["New York Jets", 0],["Philadelphia Eagles", 0],["Pittsburgh Steelers", 0],["San Francisco 49ers", 0],["Seattle Seahawks", 0],["Tampa Bay Buccaneers", 0],["Tennessee Titans", 0],["Washington Football Team", 0]]
 
-players = 400 #the amount of players
-step = 0 #is re-declared lower, is just here to remind me that it is here
-weeks = 18 #the amount of weeks this goes on for (change to 18 when done with dev)
+shortNames = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DET", " GB", "HOU", "IND", "JAX", " KC", "MIA", "MIN", " NE", " NO", "NYG", "NYJ", " LV", "PHI", "PIT", "LAC", " SF", "SEA", "LAR", " TB", "TEN", "WAS"] 
+
+players = 0 #counts up (simulating another person getting a ticket)
+step = 0 #is re-declared lower, its just here to remind me that it is here
+weeks = 17 #the amount of weeks this goes on for (change to 17 when done with dev)
 playerScores = [] # [[234, 26, 234, 567], [235, 235, 235]...]
 
 for i in range(weeks):
@@ -21,18 +23,28 @@ for teamInfo in allTeamStats:
 combinations = list(itertools.combinations(range(len(allTeamStats)), 3)) #generate the list of all combinations
 
 step = int(len(combinations)/weeks) #the amount of combinations skipped for every week (starts from the player's starting number) **this has to be less than combinations/weeks or the player could have multible of the same combinations (even though its basicly impossible)
-player_step = int((len(combinations) - weeks)/players) #same just a step for each player
+estimated_players = int(input("Estimated amount of players:\n>> "))
+player_step = int(len(combinations)/estimated_players) #same just a step for each player
+estimated_players = int(len(combinations)/player_step)
+print("estimated players udjusted to", estimated_players, "to fit better")
+input("(press enter)")
+
+#print(len(combinations), "/", 
 
 #debug
 print("Format: [Team_Name, Score] \n", allTeamStats)
-print("\n", len(combinations), "combinations")#"\n", combinations) #display all combinations (a lot)
-print("\n# of players:", players)
+print("\n" + str(len(combinations)), "combinations")#"\n", combinations) #display all combinations (a lot)
+#print("# of players:", players) #not an exact amount so I removed it
 print("Per week step amount:", step)
 print("Per player step amount:", player_step, "\n")
 
 def getScores():
-  for player_ID in range(players):
-    #print("\nPlayer_ID:", player_ID) #debug
+  #player_step = int((len(combinations) - step)/(int(input("Estimated amount of players:\n>> ")) * 3))
+  command = ""
+  player_ID = 0
+  while player_ID < estimated_players:#command != "done":
+    #for player_ID in range(players):
+    print("\nPlayer_ID:", player_ID) #debug
     for week_number in range(weeks): 
       score = 0 #reset the score every time it loops over another player
       combination_ID = player_step * player_ID + week_number * step #for every player adds a player_step, for every week it adds a step, making it so everyone has different combinations no matter what
@@ -42,6 +54,11 @@ def getScores():
         score += allTeamStats[team_ID][1] #combining all the scores of the teams for that week to compare with others
       playerScores[week_number].append(score + player_ID / 10000) #add the score and the player_ID to a list so we can sort it later to find the winners
       #print("Comb_ID: " + str(combination_ID) + ",", combinations[combination_ID],"=", score) #debug
+      for team in combinations[combination_ID]:
+        print(shortNames[team] + ", ", end = '')
+      print("")
+    #command = input(">> ")
+    player_ID += 1
   return(playerScores)
 
 def getWinners(): #***** does not do ties yet
@@ -65,4 +82,6 @@ for player in winners:
   else:
     week += 1
     print("\nWeek", week, "winners:") #some ease of life stuffs
+
+#print(playerScores) debug
     
